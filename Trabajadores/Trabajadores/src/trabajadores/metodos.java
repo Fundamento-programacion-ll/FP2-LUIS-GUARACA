feacab527e046293cd4e89505ec3d325dedf83c2
luisguaraca <60004719+luisguaraca@users.noreply.github.com>
2020-01-26 22:13:22 -0500

    trbajo

---
+++ Trabajadores/Trabajadores/src/trabajadores/metodos.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajadores;
+
+/**
+ *
+ * @author antho
+ */
+public class metodos {
+    
+    empleado em = new empleado();
+    
+    
+    
+}
