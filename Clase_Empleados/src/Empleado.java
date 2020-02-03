eec8023ac2a2659892ff19e163079b6945c080a6
luisguaraca <60004719+luisguaraca@users.noreply.github.com>
2020-02-03 08:43:20 -0500

    empleado trabajo en clases

---
+++ Clase_Empleados/src/Empleado.java
@@ -0,0 +1,40 @@
+
+public class Empleado {
+    private String nombre;
+    private String apellido;
+    private int ci;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getCi() {
+        return ci;
+    }
+
+    public void setCi(int ci) {
+        this.ci = ci;
+    }
+
+    public Empleado(String nombre, String apellido, int ci) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.ci = ci;
+    }
+    public double calcular_ingresos(){
+        
+    return 0;}
+    
+    }
---
+++ Clase_Empleados/src/clase_empleados/Clase_Empleados.java
@@ -0,0 +1,12 @@
+
+package clase_empleados;
+
+
+public class Clase_Empleados {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
