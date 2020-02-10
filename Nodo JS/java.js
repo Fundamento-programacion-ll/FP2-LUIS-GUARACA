3075c1cc19d930f56f19e0332cf81d8b7bcd9b21
luisguaraca <60004719+luisguaraca@users.noreply.github.com>
2020-02-10 09:52:56 -0500

    Json

---
+++ Nodo JS/java.js
@@ -0,0 +1,87 @@
+var json1={
+    nombre: "luis",
+    edad: 24,
+    casado:false,
+    mascota: "perro"};
+colegio
+    nombrecolegio:'nelson',
+    
+
+
+console.log("hola");
+console.log(json1.nombre);
+console.log(json1['perro']);
+
+var a= 'aaa';
+let b= 12;
+console.log(a);
+a=true;
+console.log(a);
+a=1.3333;
+console.log(a);
+
+var arreglo =['aa','aa',1,true,{a:1},['asd']]
+
+
+
+
+console.log(json1+jsonluis);
+var nu=null;
+console.log(json1.nombre);
+var numero=1;
+var bool=true
+console.log(json1+numero);
+console.log(undefined.numero);
+console.log(undefined+'asd');
+console.log(undefined+{asd:1});
+
+//funcion 
+var mifuncion =function() {
+    console.log('funcion 1')
+    return true
+};
+
+var mifuncion2 =()=>{
+    return'he'
+};
+
+mifuncion();
+console.log(mifuncion());
+console.log(mifuncion2());
+
+var funcion3 =(n1, n2)=>{
+    return(n1+n2)
+}
+
+var jsonfunciones={
+    suma:() =>{
+        return 2+3;
+    }
+}
+
+
+console.log(jsonfunciones.suma());
+var numero1 = 10;
+var numero2 = 9;
+
+var numeromenornumero2 =numero2 > numero;
+console.log(typeof numeromenornumero2);
+if (numeromenornumero2){
+}else{
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
