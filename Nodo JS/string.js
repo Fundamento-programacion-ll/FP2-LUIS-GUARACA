0265b2fba2670d8cb500f20eb7e4329031f4a36a
luisguaraca <60004719+luisguaraca@users.noreply.github.com>
2020-02-11 09:55:22 -0500

    String

---
+++ Nodo JS/string.js
@@ -0,0 +1,31 @@
+var nombre = 'luis';
+var tamanoletra=nombre.length;
+console.log(nombre.length);
+
+console.log(nombre.indexOf("y"));
+console.log(nombre.search("in"));
+console.log(nombre.substring(2,4));
+console.log(nombre.charAt(7));
+
+while (tn >=0){
+    b += nombre.charAt(tn)
+    console.log(b)
+    tn
+}
+var arreglosOriginalDividido = nombre.slice(-1,1)
+console.log(nombre.slice(1,6))
+console.log(nombre.split(""))
+
+var texto="hola como estas"
+console.log('antes de reemplazar')
+console.log('que','khe')
+var textModificado = texto.replace('que hace','khe')
+console.log(textModificado);
+
+var ejammplotrim ='hola'
+
+
+
+
+
+
