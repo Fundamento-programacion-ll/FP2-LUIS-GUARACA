b813753465f0e54891ed2a236b67c63a955ea710
luisguaraca <60004719+luisguaraca@users.noreply.github.com>
2020-02-20 11:11:03 -0500

    Create comparador.java

---
+++ Comparadores/src/comparadores/comparador.java
@@ -0,0 +1,12 @@
+package comparadores;
+
+import java.util.Comparator;
+
+public class comparador implements Comparator<Object>{
+
+    @Override
+    public int compare(Object t, Object t1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
